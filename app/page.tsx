68c7941cf728029a545ee884fde152dd2d41ce41
level237 <Bramslevel129@gmail.com>
2024-03-13 10:54:47 +0100

    json server and adding card component from shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ interface Recipe{
 }
 
 async function getRecipes():Promise<Recipe[]>{
-  const result=await fetch("http://localhost:4000/");
+  const result=await fetch("http://localhost:4000/recipes");
 
   return result.json();
 }
@@ -30,13 +30,20 @@ export default async function Home() {
     <div className="grid grid-cols-3 gap-8">
       {recipes.map(recipe=>(
 
-    <Card key={recipe.id}>
-              <CardHeader>
+    <Card key={recipe.id} className="flex flex-col justify-between">
+              <CardHeader className="flex-row gap-4 items-center">
                 <div>
                   <CardTitle>{recipe.title}</CardTitle>
-                  <CardDescription>{recipe.description}</CardDescription>
+                  <CardDescription>{recipe.time} mins to cook.</CardDescription>
                 </div>
               </CardHeader>
+              <CardContent>
+                {recipe.description}
+              </CardContent>
+              <CardFooter className="flex justify-between">
+                <button>View Recipe</button>
+                {recipe.vegan && <p>Vegan!</p>}
+              </CardFooter>
       </Card>
       )
         
